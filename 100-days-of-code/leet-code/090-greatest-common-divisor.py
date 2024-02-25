62512831968b5dd40edc16dc4c670ad0d82ca5ac
Hardik-Dulani <hardikdulani66@gmail.com>
2024-02-25 23:42:35 +0530

    day90-2

--- 100-days-of-code/leet-code/090-greatest-common-divisor.py
+++ 100-days-of-code/leet-code/090-greatest-common-divisor.py
@@ -9,39 +9,55 @@
 
 # Return true if it is possible to traverse between all such pairs of indices, or false otherwise.
 
-
 class Solution:
-    def dfs(self, index, visitedIndex, visitedPrime):
-        if visitedIndex[index]:
-            return
-        visitedIndex[index] = True
-
-        for prime in self.index2prime[index]:
-            if visitedPrime.get(prime, False):
-                continue
-            visitedPrime[prime] = True
-            for index1 in self.prime2index[prime]:
-                if visitedIndex[index1]:
-                    continue
-                self.dfs(index1, visitedIndex, visitedPrime)
-
     def canTraverseAllPairs(self, nums: List[int]) -> bool:
-        self.prime2index = {}
-        self.index2prime = {}
-        for i, num in enumerate(nums):
-            temp = num
-            for j in range(2, int(num ** 0.5) + 1):
-                if temp % j == 0:
-                    self.prime2index.setdefault(j, []).append(i)
-                    self.index2prime.setdefault(i, []).append(j)
-                    while temp % j == 0:
-                        temp //= j
-            if temp > 1:
-                self.prime2index.setdefault(temp, []).append(i)
-                self.index2prime.setdefault(i, []).append(temp)
+        if len(nums) == 1:
+            return True
+        n = len(nums)
+        maxElement = max(nums)
+        if min(nums) == 1:
+            return False
+        factorArray = self.factorsCalculator(maxElement)
+        
+        parent = list(range(maxElement + 1))
+        rank = [1] * (maxElement + 1)
 
-        visitedIndex = [False] * len(nums)
-        visitedPrime = {}
-        self.dfs(0, visitedIndex, visitedPrime)
+        for num in nums:
+            x = num
+            while x > 1:
+                p = factorArray[x]
+                self.union(parent, rank, p, num)
+                while x % p == 0:
+                    x = x // p
 
-        return all(visitedIndex)
+        p = self.find(parent, nums[0])
+        for num in nums[1:]:
+            if self.find(parent, num) != p:
+                return False
+
+        return True
+
+    def factorsCalculator(self, n: int) -> List[int]:
+        dp = list(range(n + 2))
+        for i in range(2, n + 1):
+            if dp[i] == i:
+                for j in range(i * 2, n + 1, i):
+                    if dp[j] == j:
+                        dp[j] = i
+        return dp
+
+    def find(self, parent: List[int], a: int) -> int:
+        if parent[a] == a:
+            return a
+        parent[a] = self.find(parent, parent[a])
+        return parent[a]
+
+    def union(self, parent: List[int], rank: List[int], a: int, b: int) -> None:
+        a = self.find(parent, a)
+        b = self.find(parent, b)
+        if a == b:
+            return
+        if rank[a] < rank[b]:
+            a, b = b, a
+        parent[b] = a
+        rank[a] += rank[b]
